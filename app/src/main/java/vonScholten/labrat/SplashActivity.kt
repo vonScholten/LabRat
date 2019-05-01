3d477ffcb4d96371bd09f7dfa9de674bf65dea50
Frederik von Scholten <22447372+vonScholten@users.noreply.github.com>
2019-05-01 12:18:16 +0200

    Simple splash launcher screen

---
+++ app/src/main/java/vonScholten/labrat/SplashActivity.kt
@@ -0,0 +1,28 @@
+package vonScholten.labrat
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val background = object : Thread() {
+            override fun run() {
+                super.run()
+                try { //try to sleep and then launch
+                    sleep(5000) //sleep for 5000 ms
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                catch (e: Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
