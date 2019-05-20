4906c28a980cba79ac048ba06705e49077099509
Frederik von Scholten <22447372+vonScholten@users.noreply.github.com>
2019-05-20 17:31:56 +0200

    sign in and sign up

--- app/src/main/java/vonScholten/labrat/LoginActivity.kt
+++ app/src/main/java/vonScholten/labrat/LoginActivity.kt
@@ -3,6 +3,8 @@ package vonScholten.labrat
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -12,16 +14,36 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         login_button.setOnClickListener {
-            val email = login_editEmail.text.toString() //extract email address
-            Log.d("LoginActivity", "email -> $email")
-
-            val password = login_editEmail.text.toString() //extract password
-            Log.d("LoginActivity", "password -> $password")
+            login()
         }
 
         dont_have_an_account_textView.setOnClickListener {
-            Log.d("LoginActivity", "don't have an account")
+            Log.d("LoginActivity", "Don't have an account")
             finish() //finish the activity and return
         }
     }
+
+    private fun login(){
+        val email = login_editEmail.text.toString() //extract email address
+        Log.d("Login", "Email $email")
+
+        val password = login_editPassword.text.toString() //extract password
+        Log.d("Login", "Password: $password")
+
+        if(email.isEmpty() || password.isEmpty()) return //make sure fields is not empty
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if(!it.isSuccessful) return@addOnCompleteListener //if unsuccessful
+                else {
+                    Log.d("MainActivity", "Successfully signed in user with uid: ${it.result!!.user.uid}")
+                }
+            }
+            .addOnFailureListener {
+                Log.d("MainActivity", "Authentication failed: ${it.message}")
+                Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+
 }
--- app/src/main/java/vonScholten/labrat/MainActivity.kt
+++ app/src/main/java/vonScholten/labrat/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,20 +15,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         register_button.setOnClickListener {
-            val email = register_editEmail.text.toString() //extract email address
-            Log.d("MainActivity", "email -> $email")
-
-            val password = register_editPassword.text.toString() //extract password
-            Log.d("MainActivity", "password -> $password")
+            register()
         }
 
         already_have_account_textView.setOnClickListener {
-            Log.d("MainActivity", "already have an account")
+            Log.d("MainActivity", "Already have an account")
 
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
-
     }
+
+    private fun register() {
+        val email = register_editEmail.text.toString() //extract email address
+        Log.d("Main", "Email -> $email")
+
+        val password = register_editPassword.text.toString() //extract password
+        Log.d("Main", "Password -> $password")
+
+        if (email.isEmpty() || password.isEmpty()) return //make sure fields is not empty
+
+        //firebase authentication create user using email and password
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if(!it.isSuccessful) return@addOnCompleteListener //if unsuccessful
+                else Log.d("Main", "Successfully created user with uid: ${it.result!!.user.uid}")
+            }
+            .addOnFailureListener{
+                Log.d("Main", "Failed to create user: ${it.message}")
+                Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+            }
+    }
+
 }
